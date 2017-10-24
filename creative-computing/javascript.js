fe43e085bf3eaf51e6703a99b2c4d3c349dfeab7
cheoh966 <cheoh966@users.noreply.github.com>
2017-10-24 19:46:24 -0400

    commit

---
+++ creative-computing/javascript.js
@@ -0,0 +1,18 @@
+var animals = [
+	"cat",
+	"dog",
+	"pig",
+	"toucan"
+]
+for (var i=0; i<animals.length; i+1) {
+	console.log(i)
+	console.log(animals[i]);
+}
+console.log("done");
+
+for (var i=0; i<animals.length; i+1) {
+	var animal = animal[i];
+	console.log(",img src="+ animal + ".jpg'/>")
+
+var greeting = "hello world"
+document.write(greeting); 
