cfce6ada08177e2222d1d3feb0a01eba4160eed1
cheoh966 <cheoh966@users.noreply.github.com>
2017-11-03 17:14:09 -0400

    homepage
    final

---
+++ creative-computing/clock.js
@@ -0,0 +1,38 @@
+function updateTime() {
+	
+	//Create&pick a place on the page for the time
+	var clockHolder = document.getElementById("planet")
+
+	//Get the current time
+	var now = new Date()
+	var seconds = now.getSeconds()
+	now = now.toTimeString()
+	/* OR
+	var now = new Date().toTimeString() */
+
+	console.log(now)
+	console.log(seconds)
+
+	//Figure out the color
+	if (seconds < 20) {
+		// red 
+		clockHolder.style.color = "red"
+	}	else if (seconds < 40){
+		//green
+		clockHolder.style.color = "green"
+	}	else {
+		//blue
+		clockHolder.style.color = "blue"
+	}
+
+	//Put the current time on the page
+	//element=clockholder value=now
+	clockHolder.innerHTML = now
+}
+	
+//function,how often.referencing
+setInterval(updateTime,1000)
+
+
+
+
