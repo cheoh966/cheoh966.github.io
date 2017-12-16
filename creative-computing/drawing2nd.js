bc4590d168c1ab2af738dd05dfd21728903e2574
cheoh966 <cheoh966@users.noreply.github.com>
2017-12-15 21:04:32 -0500

    asdf

--- creative-computing/drawing2nd.js
+++ creative-computing/drawing2nd.js
@@ -1,12 +1,11 @@
-// Little Canvas things
+
 var canvas = document.querySelector("#canvas"),
     ctx = canvas.getContext('2d');
 
-// Set Canvas to be window size
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Configuration, Play with these
 var config = {
   particleNumber: 800,
   maxParticleSize: 10,
@@ -14,48 +13,36 @@ var config = {
   colorVariation: 50
 };
 
-// Colors
 var colorPalette = {
     bg: {r:12,g:9,b:29},
     matter: [
-      {r:36,g:18,b:42}, // darkPRPL
-      {r:78,g:36,b:42}, // rockDust
-      {r:252,g:178,b:96}, // solorFlare
-      {r:253,g:238,b:152} // totesASun
+      {r:36,g:18,b:42}, 
+      {r:78,g:36,b:42}, 
+      {r:252,g:178,b:96}, 
+      {r:253,g:238,b:152} 
     ]
 };
 
-// Some Variables hanging out
 var particles = [],
     centerX = canvas.width / 2,
     centerY = canvas.height / 2,
     drawBg,
 
-// Draws the background for the canvas, because space
 drawBg = function (ctx, color) {
     ctx.fillStyle = "rgb(" + color.r + "," + color.g + "," + color.b + ")";
     ctx.fillRect(0,0,canvas.width,canvas.height);
 };
 
-// Particle Constructor
 var Particle = function (x, y) {
-    // X Coordinate
     this.x = x || Math.round(Math.random() * canvas.width);
-    // Y Coordinate
     this.y = y || Math.round(Math.random() * canvas.height);
-    // Radius of the space dust
     this.r = Math.ceil(Math.random() * config.maxParticleSize);
-    // Color of the rock, given some randomness
     this.c = colorVariation(colorPalette.matter[Math.floor(Math.random() * colorPalette.matter.length)],true );
-    // Speed of which the rock travels
     this.s = Math.pow(Math.ceil(Math.random() * config.maxSpeed), .7);
-    // Direction the Rock flies
     this.d = Math.round(Math.random() * 360);
 };
 
-// Provides some nice color variation
-// Accepts an rgba object
-// returns a modified rgba object or a rgba string if true is passed in for argument 2
+
 var colorVariation = function (color, returnString) {
     var r,g,b,a, variation;
     r = Math.round(((Math.random() * config.colorVariation) - (config.colorVariation/2)) + color.r);
@@ -69,16 +56,14 @@ var colorVariation = function (color, returnString) {
     }
 };
 
-// Used to find the rocks next point in space, accounting for speed and direction
 var updateParticleModel = function (p) {
-    var a = 180 - (p.d + 90); // find the 3rd angle
+    var a = 180 - (p.d + 90); 
     p.d > 0 && p.d < 180 ? p.x += p.s * Math.sin(p.d) / Math.sin(p.s) : p.x -= p.s * Math.sin(p.d) / Math.sin(p.s);
     p.d > 90 && p.d < 270 ? p.y += p.s * Math.sin(a) / Math.sin(p.s) : p.y -= p.s * Math.sin(a) / Math.sin(p.s);
     return p;
 };
 
-// Just the function that physically draws the particles
-// Physically? sure why not, physically.
+
 var drawParticle = function (x, y, r, c) {
     ctx.beginPath();
     ctx.fillStyle = c;
@@ -87,7 +72,6 @@ var drawParticle = function (x, y, r, c) {
     ctx.closePath();
 };
 
-// Remove particles that aren't on the canvas
 var cleanUpArray = function () {
     particles = particles.filter((p) => { 
       return (p.x > -100 && p.y > -100); 
@@ -104,7 +88,6 @@ var initParticles = function (numParticles, x, y) {
     });
 };
 
-// That thing
 window.requestAnimFrame = (function() {
   return window.requestAnimationFrame ||
      window.webkitRequestAnimationFrame ||
@@ -115,32 +98,23 @@ window.requestAnimFrame = (function() {
 })();
 
 
-// Our Frame function
 var frame = function () {
-  // Draw background first
   drawBg(ctx, colorPalette.bg);
-  // Update Particle models to new position
   particles.map((p) => {
     return updateParticleModel(p);
   });
-  // Draw em'
   particles.forEach((p) => {
       drawParticle(p.x, p.y, p.r, p.c);
   });
-  // Play the same song? Ok!
   window.requestAnimFrame(frame);
 };
 
-// Click listener
 document.body.addEventListener("click", function (event) {
     var x = event.clientX,
         y = event.clientY;
     cleanUpArray();
     initParticles(config.particleNumber, x, y);
 });
-
-// First Frame
 frame();
 
-// First particle explosion
 initParticles(config.particleNumber);
